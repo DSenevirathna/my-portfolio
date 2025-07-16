5a0efa7c7f83f90d62c2ee6e06c6237d9ffd90d4
Devdini Senevirathna <devdinichaniya2001@gmail.com>
2025-07-16 18:35:26 +0530

    Refactor code structure for improved readability and maintainability

---
+++ components/ui/Photo.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+import {motion} from 'framer-motion'
+import Image  from 'next/image'
+
+const Photo = () => {
+  return (
+    <div className='w-full h-full relative'>
+      photo
+    </div>
+  )
+}
+
+export default Photo
