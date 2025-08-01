03b63e614144aba62e7f389d9296925304a5b147
Devdini Senevirathna <devdinichaniya2001@gmail.com>
2025-08-01 17:57:40 +0530

    feat: enhance contact page with form and information display
    - Added a contact form with input fields for first name, last name, email, and phone number.
    - Integrated a service selection dropdown using the Select component.
    - Included contact information section with icons for phone, email, and address.
    - Implemented motion effects for a smoother user experience on page load.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,10 +1,98 @@
+"use client"
+
 import React from 'react'
+import {Button} from '@/components/ui/button'
+import {Input} from '@/components/ui/input'
+import {Textarea} from '@/components/ui/textarea'
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+
+import { FaPhoneAlt , FaEnvelope , FaMapMarkedAlt} from 'react-icons/fa'
+
+
+const info =[
+  {
+    icon: <FaPhoneAlt/>,
+    title:"Phone",
+    description: "(+94) 76 547 0666"
+  },
+  {
+    icon: <FaEnvelope/>,
+    title:"Email",
+    description: "devdinichaniya2001@gmail.com"
+  },
+  {
+    icon: <FaMapMarkedAlt/>,
+    title:"Address",
+    description: "Kandy, Sri Lanka"
+  }
+]
+
+import {motion} from 'framer-motion'
+
 
 const contact = () => {
   return (
-    <div>
-      
+   <motion.section
+    initial={{opacity: 0}}
+    animate={{
+      opacity:1,
+      transition : {delay :2.4 , duration: 0.4 , ease:"easeIn"},
+    }}
+    className='py-6'
+   >
+    <div className='container mx-auto'>
+     <div className='flex flex-col xl:flex-row gap-[30px]'>
+       {/*form*/}
+       <div className='xl:h-[54%] order-2 xl:order-none' >
+          <form className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
+            <h3 className='text-4xl text-[#F5921F]'>Let's work together</h3>
+            <p className='text-white/60'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, animi cum quia optio totam itaque non magni possimus accusamus dolorem doloremque. Ab ipsa molestias rerum laborum animi ipsam aspernatur est!</p>
+            {/*input*/}
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+              <Input type="firstname" placeholder="First Name" />
+              <Input type="lastname" placeholder="Last Name" />
+              <Input type="email" placeholder="Email" />
+              <Input type="phone" placeholder="Phone Number" />
+
+            </div>
+            {/*select*/}
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder=" Select a Service"/>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Select a Service</SelectLabel>
+                  <SelectItem value="est">Web Development</SelectItem>
+                  <SelectItem value="cst">UI/UX Design</SelectItem>
+                  
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
+
+
+            
+          </form>
+       </div>
+        {/*info*/}
+        <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-none  mb-8 xl:mb-0'>info</div>
+
+     </div>
+
+
     </div>
+
+   </motion.section>
   )
 }
 
