f858283394fa80a5b928a9177d49940255761af4
Devdini Senevirathna <devdinichaniya2001@gmail.com>
2025-08-02 12:01:12 +0530

    feat: update profile image and enhance Photo component styling
    - Replaced profile picture with a new image asset for a more personal touch.
    - Adjusted dimensions and positioning of the image and surrounding elements for improved layout.
    - Enhanced SVG circle animation properties for a smoother visual effect.
    - Updated class names for better responsiveness and alignment.

--- components/ui/Photo.jsx
+++ components/ui/Photo.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import Image  from 'next/image'
-import profilePic from '@/public/profilePic.jpeg' 
+import profilePic from '@/public/myPic.png' 
 
 const Photo = () => {
   return (
@@ -24,7 +24,7 @@ const Photo = () => {
                 opacity: 1,
                 transition : {delay : 2.4 , duration: 0.4 , ease:"easeInOut"},
               }}
-              className="absolute inset-0 m-auto w-[298px] h-[298px] xl:w-[400px] xl:h-[400px] flex items-center justify-center"
+              className="relative w-[310px] h-[310px] xl:w-[420px] xl:h-[420px] flex items-center justify-center"
             >
               <Image
                 src={profilePic}
@@ -32,12 +32,13 @@ const Photo = () => {
                 quality={100}
                 fill
                 alt='my-photo'
-                className='rounded-full object-cover'
+                className=' object-cover rounded-full pb-0 pr-10'
+                style={{ objectPosition: 'center 100%' }}
               />
             </motion.div>
 
              {/*circle*/}
-             <motion.svg className="w-[310px] h-[310px] xl:w-[420px] xl:h-[420px]" 
+             <motion.svg className="absolute inset-0 w-[310px] h-[310px] xl:w-[420px] xl:h-[420px]" 
               fill="transparent"
               viewBox="0 0 506 506"
               xmlns="http://www.w3.org/2000/svg"
@@ -45,11 +46,11 @@ const Photo = () => {
 
              >
               <motion.circle
-                cx="253"
-                cy="253"
-                r="250"
+                cx="233"
+                cy="283"
+                r="220"
                 stroke="#F5921F"
-                strokeWidth="4"
+                strokeWidth="5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 initial= {{strokeDasharray: "24 10 0 0 "}}
@@ -58,7 +59,7 @@ const Photo = () => {
                   rotate: [120 , 360],
                 }}
                 transition={{
-                  duration:10,
+                  duration:8,
                   repeat: Infinity,
                   repeatType:"reverse"
                 }}
